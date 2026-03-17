2855283bd558d53658994aec6e582202f8eac346
Nick Cossman <nickcossman1983@gmail.com>
2026-03-17 18:49:11 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Toaster } from "@/components/ui/toaster"
 import { base44 } from '@/api/base44Client';
 import { QueryClientProvider } from '@tanstack/react-query'
